1bee08840614d8f062a1152b5e1d2667c1b801e3
Saraschandraa Muralisekar <saraschandraa.naidu@hotmail.com>
2021-02-23 19:58:06 +0530

    Relative Layout and Scroll View Added

--- app/src/main/java/com/saras/uibasics/MainActivity.java
+++ app/src/main/java/com/saras/uibasics/MainActivity.java
@@ -3,12 +3,19 @@ package com.saras.uibasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_relative);
+
+        TextView mTvSetupWizard = findViewById(R.id.tv_setup_wizard);
+        ImageView mIvPieImage = findViewById(R.id.iv_pie_chart);
+
+
     }
 }
